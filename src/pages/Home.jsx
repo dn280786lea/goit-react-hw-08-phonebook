afcc339e07ef487f8cb6bc0bf1cdb3bc6c867178
dn280786lea <ekaterina.lubenets@gmail.com>
2023-12-21 20:04:53 +0100

    1
    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,9 @@
-const styles = {
-  container: {
-    minHeight: 'calc(100vh - 50px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 48,
-    textAlign: 'center',
-  },
-};
+import React from 'react';
 
 export default function Home() {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>
+    <div>
+      <h1>
         Task manager welcome page{' '}
         <span role="img" aria-label="Greeting icon">
           💁‍♀️
