4664c8a916c1e9eb13d91500b286d633f32c66f2
dn280786lea <132853931+dn280786lea@users.noreply.github.com>
2023-12-21 19:05:42 +0000

    Deploying to gh-pages from @ dn280786lea/goit-react-hw-08-phonebook@afcc339e07ef487f8cb6bc0bf1cdb3bc6c867178 🚀

---
+++ static/js/415.3aad0695.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,r,n){n.r(r),n.d(r,{default:function(){return t}});n(791);var a=n(184);function t(){return(0,a.jsx)("div",{children:(0,a.jsxs)("h1",{children:["Task manager welcome page"," ",(0,a.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=415.3aad0695.chunk.js.map
