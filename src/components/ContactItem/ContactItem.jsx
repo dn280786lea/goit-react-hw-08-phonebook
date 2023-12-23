80137dc5c4e4542db6cdb8f4fbe44b9bdf731fd6
dn280786lea <ekaterina.lubenets@gmail.com>
2023-12-23 16:18:05 +0100

    up
    up

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,15 +3,19 @@ import './ContactItem.css';
 import { deleteContact } from '../../redux/operations';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import * as React from 'react';
 
 export const ContactItem = ({ name, number, id }) => {
   const dispatch = useDispatch();
+
   return (
     <div className="contact-item" key={id}>
       <Stack direction="row" spacing={2}>
-        <Avatar alt="{name}" src="/static/images/avatar/1.jpg" />
-        <Avatar alt="{name}" src="/static/images/avatar/2.jpg" />
-        <Avatar alt="{name}r" src="/static/images/avatar/3.jpg" />
+        <Avatar
+          className="avatar"
+          alt={name}
+          src="/static/images/avatar/3.jpg"
+        />
       </Stack>
       <span className="item">{name} </span>
       <span className="item">{number} </span>
