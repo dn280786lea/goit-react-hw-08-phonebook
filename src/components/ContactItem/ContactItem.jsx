522a4160b983c9c3786d3f3d3b1ee2d2eb80be31
dn280786lea <ekaterina.lubenets@gmail.com>
2023-12-25 17:28:04 +0100

    up
    up

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,21 +1,41 @@
 import { useDispatch } from 'react-redux';
 import './ContactItem.css';
 import { deleteContact } from '../../redux/operations';
-import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import * as React from 'react';
 
+const RandomImageComponent = () => {
+  const images = [
+    'https://bit.ly/tioluwani-kolawole',
+    'https://bit.ly/dan-abramov',
+    'https://bit.ly/kent-c-dodds',
+    'https://bit.ly/prosper-baba',
+  ];
+  const [randomImage, setRandomImage] = React.useState('');
+
+  const getRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomIndex];
+    setRandomImage(selectedImage);
+  };
+
+  React.useEffect(() => {
+    getRandomImage();
+  }, []);
+
+  return randomImage;
+};
+
 export const ContactItem = ({ name, number, id }) => {
   const dispatch = useDispatch();
+  const randomImage = RandomImageComponent();
 
   return (
     <div className="contact-item" key={id}>
       <Stack direction="row" spacing={2}>
-        <Avatar
-          className="avatar"
-          alt={name}
-          src="https://bit.ly/dan-abramov"
-        />
+        {randomImage && (
+          <img src={randomImage} alt="avatar" className="avatar" />
+        )}
       </Stack>
       <span className="item">{name} </span>
       <span className="item">{number} </span>
